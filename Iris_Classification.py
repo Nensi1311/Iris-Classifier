d4d2f0b076b8dc1d139f965eecf94036aca5dae9
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-07 21:57:22 +0530

    Update Iris_Classification.py

--- Iris_Classification.py
+++ Iris_Classification.py
@@ -1,1 +1,14 @@
+# pandas - working with data set, load data from a CSV file, grouping data by a specific column
+import pandas as pd
 
+# working with numeric data, provides efficient operations on arrays of homogeneous data
+import numpy as np
+
+# is a library for making statistical graphics in Python
+import seaborn as sns
+
+# displaying data and creating static, animated and interactive plots
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
