298e2725f93c41ddc43c3a45774bf87295e548e1
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 18:25:26 +0530

    Update src.py

--- src.py
+++ src.py
@@ -10,3 +10,18 @@ try:
 except Exception as e:
     print(f"Can't Save the model : {e}")
   
+load_model = pickle.load(open(filename, "rb"))
+
+SL = float(input("Enter Sepal Length :"))
+SL
+
+SW = float(input("Enter Sepal Width :"))
+SW
+
+PL = float(input("Enter Petal Length :"))
+PL
+
+PW = float(input("Enter Petal Width :"))
+PW
+
+print("Iris flower Species is : ",load_model.predict([[SL, SW, PL, PW]]))
