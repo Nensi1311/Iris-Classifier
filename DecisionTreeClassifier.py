119961466b7431e9750b224b9a8f8bab72ac2ddc
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 18:23:15 +0530

    Update DecisionTreeClassifier.py

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -1,1 +1,7 @@
+from sklearn.tree import DecisionTreeClassifier
 
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+print("Decision Tree Accuracy is :", model.score(x_test, y_test)*100)
+
+model.fit(x_train.values, y_train.values)
