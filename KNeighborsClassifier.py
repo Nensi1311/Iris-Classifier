566f9238ab878123606cfdf5fe09288b5d4f5e0d
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 18:21:15 +0530

    Update KNeighborsClassifier.py

--- KNeighborsClassifier.py
+++ KNeighborsClassifier.py
@@ -1,1 +1,7 @@
+from sklearn.neighbors import KNeighborsClassifier
 
+model = KNeighborsClassifier()
+model.fit(x_train, y_train)
+print("K-nearest neighbors Accuracy is :", model.score(x_test, y_test)*100)
+
+model.fit(x_train.values, y_train.values)
