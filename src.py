83e0fc24cf8597b92545baad8f56a33606f0fd05
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-10-21 18:24:16 +0530

    Update src.py

--- src.py
+++ src.py
@@ -1,1 +1,12 @@
+import pickle
+filename = "iris"
+pickle.dump(model, open(filename, "wb"))
 
+filename = "iris"
+try:
+    with open(filename, "wb") as file:
+        pickle.dump(model, file)
+    print("Model Saved Successfully!")
+except Exception as e:
+    print(f"Can't Save the model : {e}")
+  
